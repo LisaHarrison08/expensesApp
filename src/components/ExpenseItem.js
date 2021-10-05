6a8232f34608eb9431889362b5eb483cede21a61
Lisa Harrison <lmharrison0810@gmail.com>
2021-10-05 10:44:13 -0400

    expenses component css added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,15 @@
+// 
+
+import './ExpenseItem.css';
+
 function ExpenseItem() {
   return (
-    <div>
+    // class attribute is written as className - class is a reserved word in javaScript
+    <div className="expense-item">
       <div>Date</div>
-      <div>
+      <div className="expense-item__description">
         <h2>Title</h2>
-        <div>Amount</div>
+        <div className="expense-item__price">$200.50</div>
       </div>
     </div>
   );
