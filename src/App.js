db20250a7d6d1fe3bb84e358780691e4dff440ac
Lisa Harrison <lmharrison0810@gmail.com>
2021-10-13 23:22:38 -0400

    included logo component and facerec component to be reviewed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Navigation from './components/Navigation/Navigation';
 import Signin from './components/Signin/Signin';
 import Register from './components/Register/Register';
+import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
@@ -80,9 +82,9 @@ class App extends Component {
     app.models
       .predict(
         // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
-        // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
+        // A good way to check if the model you are using is up, is to check on the clarifai website. For example,
         // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
-        // If that isn't working, then that means you will have to wait until their servers are back up. Another solution
+        // If that isn't working, you will have to wait until their servers are back up. Another solution
         // is to use a different version of their model that works like: `c0c0ac362b03416da06ab3fa36fb58e3`
         // so you would change from:
         // .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
